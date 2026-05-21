98dbd38edcb5e7c5656f036aaeb5769dc339f8e2
BeD <benjamin.desbouchages@gmail.com>
2026-05-21 11:56:46 +0200

    feat: Enhance PM2 integration for graceful lifecycle management
    The application now sends a "ready" signal to PM2 upon startup,
    ensuring PM2 correctly detects when the application is ready to serve
    requests. It also implements handlers for `SIGTERM` and `SIGINT`
    signals to gracefully close the Nuxt server, preventing abrupt
    termination during restarts or shutdowns managed by PM2.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,31 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("listen", (server, { port }) => {
+        console.log(`Server is listening on port ${port}`);
+
+        // Emit ready event for PM2
+        if (process.send) process.send("ready");
+        else console.log("process.send n'est pas disponible.");
+
+        process.on("SIGTERM", () => {
+          console.log("SIGTERM received");
+
+          server.close(() => {
+            console.log("Server closed");
+          }); // Arrêtez le serveur Nuxt
+        });
+
+        process.on("SIGINT", () => {
+          console.log("SIGINT received");
+
+          server.close(() => {
+            console.log("Server closed");
+          }); // Arrêtez le serveur Nuxt
+        });
+      });
+    },
     "@nuxt/eslint",
     "@nuxtjs/tailwindcss",
     "vuetify-nuxt-module",
